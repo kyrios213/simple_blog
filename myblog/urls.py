770038c2f4cf32892a327907c8b8c73ca6c03777
Lance Pidor <kyrios213@gmail.com>
2021-05-04 12:03:27 +0800

    Added datefield to db and added edit and delete to posts

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomeView, PostDetailView, PostCreateView
+from .views import HomeView, PostDetailView, PostCreateView, PostEditView, PostDeleteView
 
 app_name = 'myblog'
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('article/<int:pk>', PostDetailView.as_view(), name='detail'),
     path('add_post/', PostCreateView.as_view(), name='add_post'),
+    path('article/edit/<int:pk>', PostEditView.as_view(), name='edit'),
+    path('article/delete/<int:pk>', PostDeleteView.as_view(), name='delete'),
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.views import generic
+from django.urls import reverse_lazy
 
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, EditForm
 
 class HomeView(generic.ListView):
     model = Post
     template_name = 'myblog/home.html'
-    queryset = Post.objects.order_by('-id').all()
+    queryset = Post.objects.order_by('-post_date').all()
 
 
 class PostDetailView(generic.DetailView):
@@ -19,3 +20,15 @@ class PostCreateView(generic.CreateView):
     model = Post
     form_class = PostForm
     template_name = 'myblog/add_post.html'
+
+
+class PostEditView(generic.UpdateView):
+    model = Post
+    form_class = EditForm
+    template_name = 'myblog/update_post.html'
+
+
+class PostDeleteView(generic.DeleteView):
+    model = Post
+    template_name = 'myblog/delete_post.html'
+    success_url = reverse_lazy('myblog:home')
