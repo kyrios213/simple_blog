c3094fa8854767caf1dbe51886be97ee179be63c
Lance Pidor <kyrios213@gmail.com>
2021-05-13 11:02:10 +0800

    Added static files and profile page

--- bloggers/urls.py
+++ bloggers/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserRegisterView, UserEditView, PasswordsChangeView
+from .views import UserRegisterView, UserEditView, PasswordsChangeView, ShowProfilePageView
 from django.contrib.auth import views as auth_views
 
 app_name = 'bloggers'
@@ -8,4 +8,5 @@ urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
     path('password/', PasswordsChangeView.as_view(template_name='registration/change_password.html'), name='change_pass'),
+    path('<int:pk>/profile', ShowProfilePageView.as_view(), name='profile_page'),
 ]
--- bloggers/views.py
+++ bloggers/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views import generic
 from django.contrib.auth.forms import PasswordChangeForm, UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
 from .forms import PasswordsChangeForm, SignUpForm, EditProfileForm
 from django.contrib.auth.views import PasswordChangeView
 
+from myblog.models import Profile
 
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
@@ -24,3 +25,16 @@ class UserEditView(generic.UpdateView):
 class PasswordsChangeView(PasswordChangeView):
     form_class = PasswordsChangeForm
     success_url = reverse_lazy('bloggers:edit_profile')
+
+
+class ShowProfilePageView(generic.DetailView):
+    model = Profile
+    template_name = "registration/user_profile.html"
+
+    def get_context_data(self, *args, **kwargs):
+        profile = Profile.objects.all()
+        context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
+        page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
+        
+        context['profile'] = page_user
+        return context 
